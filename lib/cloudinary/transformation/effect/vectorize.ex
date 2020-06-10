e04f56b68e0bc2d4c19be7d33cf81bdc777b0731
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-06-10 09:43:58 +0900

    fix: legacy elixir versions support
    Avoid using is_map_key/2 guard since it comes from Elixir version 1.10.0

--- lib/cloudinary/transformation/effect/vectorize.ex
+++ lib/cloudinary/transformation/effect/vectorize.ex
@@ -5,9 +5,6 @@ defmodule Cloudinary.Transformation.Effect.Vectorize do
   defguardp is_despeckle(despeckl) when (despeckl <= 1 and despeckl >= 0) or despeckl in 2..100
   defguardp is_paths(paths) when paths <= 100 and paths >= 0
   defguardp is_corners(corners) when corners <= 100 and corners >= 0
-  defguardp is_not_map_key(map, key) when not is_map_key(map, key)
-  defguardp is_not_map_key(map, k1, k2) when is_not_map_key(map, k1) and is_not_map_key(map, k2)
-  defguardp is_not_map_key(m, k1, k2, k3) when is_not_map_key(m, k1, k2) and is_not_map_key(m, k3)
 
   @spec to_url_string(%{
           optional(:colors) => 2..30,
@@ -26,40 +23,99 @@ defmodule Cloudinary.Transformation.Effect.Vectorize do
     "vectorize:#{c}:#{t}:#{s}:#{p}"
   end
 
-  def to_url_string(%{colors: c, detail: t, despeckle: s} = options)
-      when is_colors(c) and is_detail(t) and is_despeckle(s) and is_not_map_key(options, :corners) do
+  def to_url_string(%{colors: c, detail: t, despeckle: s, corners: r})
+      when is_colors(c) and is_detail(t) and is_despeckle(s) and is_corners(r) do
+    "vectorize:colors:#{c}:detail:#{t}:despeckle:#{s}:corners:#{r}"
+  end
+
+  def to_url_string(%{colors: c, detail: t, paths: p, corners: r})
+      when is_colors(c) and is_detail(t) and is_paths(p) and is_corners(r) do
+    "vectorize:colors:#{c}:detail:#{t}:paths:#{p}:corners:#{r}"
+  end
+
+  def to_url_string(%{colors: c, despeckle: s, paths: p, corners: r})
+      when is_colors(c) and is_despeckle(s) and is_paths(p) and is_corners(r) do
+    "vectorize:colors:#{c}:despeckle:#{s}:paths:#{p}:corners:#{r}"
+  end
+
+  def to_url_string(%{detail: t, despeckle: s, paths: p, corners: r})
+      when is_detail(t) and is_despeckle(s) and is_paths(p) and is_corners(r) do
+    "vectorize:detail:#{t}:despeckle:#{s}:paths:#{p}:corners:#{r}"
+  end
+
+  def to_url_string(%{colors: c, detail: t, despeckle: s})
+      when is_colors(c) and is_detail(t) and is_despeckle(s) do
     "vectorize:#{c}:#{t}:#{s}"
   end
 
-  def to_url_string(%{colors: c, detail: t} = options)
-      when is_colors(c) and is_detail(t) and is_not_map_key(options, :paths, :corners) do
+  def to_url_string(%{colors: c, detail: t, paths: p})
+      when is_colors(c) and is_detail(t) and is_paths(p) do
+    "vectorize:colors:#{c}:detail:#{t}:paths:#{p}"
+  end
+
+  def to_url_string(%{colors: c, detail: t, corners: r})
+      when is_colors(c) and is_detail(t) and is_corners(r) do
+    "vectorize:colors:#{c}:detail:#{t}:corners:#{r}"
+  end
+
+  def to_url_string(%{colors: c, despeckle: s, paths: p})
+      when is_colors(c) and is_despeckle(s) and is_paths(p) do
+    "vectorize:colors:#{c}:despeckle:#{s}:paths:#{p}"
+  end
+
+  def to_url_string(%{colors: c, despeckle: s, corners: r})
+      when is_colors(c) and is_despeckle(s) and is_corners(r) do
+    "vectorize:colors:#{c}:despeckle:#{s}:corners:#{r}"
+  end
+
+  def to_url_string(%{colors: c, paths: p, corners: r})
+      when is_colors(c) and is_paths(p) and is_corners(r) do
+    "vectorize:colors:#{c}:paths:#{p}:corners:#{r}"
+  end
+
+  def to_url_string(%{colors: c, detail: t}) when is_colors(c) and is_detail(t) do
     "vectorize:#{c}:#{t}"
   end
 
-  def to_url_string(%{colors: c} = options)
-      when is_colors(c) and is_not_map_key(options, :despeckle, :paths, :corners) do
-    "vectorize:#{c}"
+  def to_url_string(%{colors: c, despeckle: s}) when is_colors(c) and is_despeckle(s) do
+    "vectorize:colors:#{c}:despeckle:#{s}"
   end
 
-  def to_url_string(%{colors: c} = options) when is_colors(c) do
-    "#{to_url_string(Map.delete(options, :colors))}:colors:#{c}"
+  def to_url_string(%{colors: c, paths: p}) when is_colors(c) and is_paths(p) do
+    "vectorize:colors:#{c}:paths:#{p}"
   end
 
-  def to_url_string(%{detail: t} = options) when is_detail(t) do
-    "#{to_url_string(Map.delete(options, :detail))}:detail:#{t}"
+  def to_url_string(%{colors: c, corners: r}) when is_colors(c) and is_corners(r) do
+    "vectorize:colors:#{c}:corners:#{r}"
   end
 
-  def to_url_string(%{despeckle: s} = options) when is_despeckle(s) do
-    "#{to_url_string(Map.delete(options, :despeckle))}:despeckle:#{s}"
+  def to_url_string(%{detail: t, despeckle: s}) when is_detail(t) and is_despeckle(s) do
+    "vectorize:detail:#{t}:despeckle:#{s}"
   end
 
-  def to_url_string(%{paths: p} = options) when is_paths(p) do
-    "#{to_url_string(Map.delete(options, :paths))}:paths:#{p}"
+  def to_url_string(%{detail: t, paths: p}) when is_detail(t) and is_paths(p) do
+    "vectorize:detail:#{t}:paths:#{p}"
   end
 
-  def to_url_string(%{corners: r} = options) when is_corners(r) do
-    "#{to_url_string(Map.delete(options, :corners))}:corners:#{r}"
+  def to_url_string(%{detail: t, corners: r}) when is_detail(t) and is_corners(r) do
+    "vectorize:detail:#{t}:corners:#{r}"
   end
 
-  def to_url_string(%{}), do: "vectorize"
+  def to_url_string(%{despeckle: s, paths: p}) when is_despeckle(s) and is_paths(p) do
+    "vectorize:despeckle:#{s}:paths:#{p}"
+  end
+
+  def to_url_string(%{despeckle: s, corners: r}) when is_despeckle(s) and is_corners(r) do
+    "vectorize:despeckle:#{s}:corners:#{r}"
+  end
+
+  def to_url_string(%{paths: p, corners: r}) when is_paths(p) and is_corners(r) do
+    "vectorize:paths:#{p}:corners:#{r}"
+  end
+
+  def to_url_string(%{colors: c}) when is_colors(c), do: "vectorize:#{c}"
+  def to_url_string(%{detail: t}) when is_detail(t), do: "vectorize:detail:#{t}"
+  def to_url_string(%{despeckle: s}) when is_despeckle(s), do: "vectorize:despeckle:#{s}"
+  def to_url_string(%{paths: p}) when is_paths(p), do: "vectorize:paths:#{p}"
+  def to_url_string(%{corners: r}) when is_corners(r), do: "vectorize:corners:#{r}"
 end
--- lib/cloudinary/transformation/width.ex
+++ lib/cloudinary/transformation/width.ex
@@ -2,10 +2,6 @@ defmodule Cloudinary.Transformation.Width do
   @moduledoc false
   defguardp is_width(width) when is_number(width) and width >= 0
   defguardp is_rounding_step(rounding_step) when is_number(rounding_step) and rounding_step > 0
-  defguardp without_breakpoints(options) when not is_map_key(options, :breakpoints)
-
-  defguardp without_rounding_step(options)
-            when not is_map_key(options, :rounding_step) and not is_map_key(options, :width)
 
   defguardp is_bytes_step(bytes_step)
             when is_integer(bytes_step) and bytes_step > 0 and rem(bytes_step, 1000) == 0
@@ -15,28 +11,28 @@ defmodule Cloudinary.Transformation.Width do
   @spec to_url_string(Cloudinary.Transformation.width()) :: String.t()
   def to_url_string(width) when is_width(width) or width == :auto, do: "#{width}"
 
-  def to_url_string({:auto, %{rounding_step: rounding_step, width: width} = options})
-      when is_rounding_step(rounding_step) and is_width(width) and without_breakpoints(options) do
+  def to_url_string({:auto, %{rounding_step: _, breakpoints: _}}) do
+    raise ArgumentError, ":rounding_step and :breakpoints options cannot be put at same time"
+  end
+
+  def to_url_string({:auto, %{width: _, breakpoints: _}}) do
+    raise ArgumentError, ":width and :breakpoints options cannot be put at same time"
+  end
+
+  def to_url_string({:auto, %{rounding_step: rounding_step, width: width}})
+      when is_rounding_step(rounding_step) and is_width(width) do
     "auto:#{rounding_step}:#{width}"
   end
 
-  def to_url_string({:auto, %{rounding_step: rounding_step} = options})
-      when is_rounding_step(rounding_step) and without_breakpoints(options) do
+  def to_url_string({:auto, %{rounding_step: rounding_step}})
+      when is_rounding_step(rounding_step) do
     "auto:#{rounding_step}"
   end
 
-  def to_url_string({:auto, %{width: width} = options})
-      when is_width(width) and without_breakpoints(options) do
-    "auto:100:#{width}"
-  end
+  def to_url_string({:auto, %{width: width}}) when is_width(width), do: "auto:100:#{width}"
+  def to_url_string({:auto, %{breakpoints: true}}), do: "auto:breakpoints"
 
-  def to_url_string({:auto, %{breakpoints: true} = options})
-      when without_rounding_step(options) do
-    "auto:breakpoints"
-  end
-
-  def to_url_string({:auto, %{breakpoints: breakpoints} = options})
-      when is_map(breakpoints) and without_rounding_step(options) do
+  def to_url_string({:auto, %{breakpoints: breakpoints}}) when is_map(breakpoints) do
     "auto:#{breakpoints_to_url_string(breakpoints)}"
   end
 
