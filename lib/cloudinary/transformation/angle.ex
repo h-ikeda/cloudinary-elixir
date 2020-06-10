098ef55d6a527e7bca5bd9656c3e356af0dd145e
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-06-10 23:33:30 +0900

    refactor: more programatic conversion

--- lib/cloudinary/transformation/angle.ex
+++ lib/cloudinary/transformation/angle.ex
@@ -4,25 +4,12 @@ defmodule Cloudinary.Transformation.Angle do
 
   @spec to_url_string(Cloudinary.Transformation.angle()) :: String.t()
   def to_url_string(angle) when is_number(angle) or is_mode(angle), do: "#{angle}"
-  def to_url_string([mode]) when is_mode(mode), do: "#{mode}"
 
-  def to_url_string([mode1, mode2]) when is_mode(mode1) and is_mode(mode2) do
-    "#{mode1}.#{mode2}"
-  end
-
-  def to_url_string([mode1, mode2, mode3])
-      when is_mode(mode1) and is_mode(mode2) and is_mode(mode3) do
-    "#{mode1}.#{mode2}.#{mode3}"
-  end
-
-  def to_url_string([mode1, mode2, mode3, mode4])
-      when is_mode(mode1) and is_mode(mode2) and is_mode(mode3) and is_mode(mode4) do
-    "#{mode1}.#{mode2}.#{mode3}.#{mode4}"
-  end
-
-  def to_url_string([mode1, mode2, mode3, mode4, mode5])
-      when is_mode(mode1) and
-             is_mode(mode2) and is_mode(mode3) and is_mode(mode4) and is_mode(mode5) do
-    "#{mode1}.#{mode2}.#{mode3}.#{mode4}.#{mode5}"
+  def to_url_string(modes) when is_list(modes) do
+    if Enum.all?(modes, fn mode -> is_mode(mode) end) do
+      Enum.join(modes, ".")
+    else
+      raise ArgumentError, "expeted a list of the angle mode atoms"
+    end
   end
 end
